1e69d9816bec71f3c5016aecaa123da88eb2e47c
juku <kuenzel.justin@t-online.de>
2018-10-04 14:04:55 +0200

    added new class I.

---
+++ src/main/java/com/jukusoft/i18n/I.java
@@ -0,0 +1,16 @@
+package com.jukusoft.i18n;
+
+public class I {
+
+    /**
+    * private constructor, so noone can create an instance of this class
+    */
+    protected I () {
+        //
+    }
+
+    public static void init () {
+        //
+    }
+
+}
---
+++ src/test/java/com/jukusoft/i18n/ITest.java
@@ -0,0 +1,10 @@
+package com.jukusoft.i18n;
+
+public class ITest {
+
+
+    public void testConstructor () {
+        //
+    }
+
+}

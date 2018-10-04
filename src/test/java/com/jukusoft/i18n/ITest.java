dc54793ada417cebd6277a9fad0ac0076254ae33
juku <kuenzel.justin@t-online.de>
2018-10-04 14:08:05 +0200

    added some junit tests for class I.

--- src/test/java/com/jukusoft/i18n/ITest.java
+++ src/test/java/com/jukusoft/i18n/ITest.java
@@ -1,10 +1,17 @@
 package com.jukusoft.i18n;
 
+import org.junit.Test;
+
 public class ITest {
 
-
+    @Test
     public void testConstructor () {
-        //
+        new I();
+    }
+
+    @Test
+    public void testInit () {
+        I.init();
     }
 
 }

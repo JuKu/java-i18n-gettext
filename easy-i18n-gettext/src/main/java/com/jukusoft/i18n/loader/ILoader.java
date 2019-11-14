30d012e40f317bc5feef8db0b6b988220b5587b6
JuKu <kuenzel.justin@t-online.de>
2019-11-15 00:30:41 +0100

    Add javadoc

--- easy-i18n-gettext/src/main/java/com/jukusoft/i18n/loader/ILoader.java
+++ easy-i18n-gettext/src/main/java/com/jukusoft/i18n/loader/ILoader.java
@@ -7,6 +7,14 @@ public interface ILoader {
 
     /**
     * load complete domain into RAM memory
+     *
+     * @param langFolder i18n folder which contains all the multi-language files
+     * @param domain domain
+     * @param locale locale to load
+     *
+     * @throws NoLangDomainFoundException if no i18n file for this domain and locale was found
+     *
+     * @return domain bundle with all i18n values which belongs to this domain
     */
     public DomainBundle load (File langFolder, String domain, Locale locale) throws NoLangDomainFoundException;
 
